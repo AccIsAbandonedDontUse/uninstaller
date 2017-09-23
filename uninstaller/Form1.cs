e545bb0f6ddb8504b5b082e6dfbbb558127b2bd9
Riachutrainer <ethankildahl@Hotmail.com>
2017-09-22 17:11:03 -0700

    Testing the upload mechanism and also doing some stuff

--- uninstaller/Form1.cs
+++ uninstaller/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,29 @@ namespace uninstaller
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
+            using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
+            {
+                foreach (string subkey_name in key.GetSubKeyNames())
+                {
+                    using (RegistryKey subkey = key.OpenSubKey(subkey_name))
+                    {
+                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                        try
+                        {
+                            listBox1.Items.Add(subkey.GetValue("DisplayName"));
+                        }
+                        catch
+                        {
+                            continue;
+                        }
+                    }
+                }
+            }
         }
     }
 }
