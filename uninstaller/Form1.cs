e83f6b13ae634bb7b54841fe2dff2183d9ef2bc7
Riachutrainer <ethankildahl@Hotmail.com>
2017-09-22 20:59:32 -0700

    Added button things

--- uninstaller/Form1.cs
+++ uninstaller/Form1.cs
@@ -17,28 +17,46 @@ namespace uninstaller
         {
             InitializeComponent();
         }
+        //global declerations
+        string selected = "";
+        string uninstallPath = "";
+        string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"; //reg key for installed programs subkeys
+        int i = 0;
+        //end of global decs.
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            string registry_key = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall";
             using (Microsoft.Win32.RegistryKey key = Registry.LocalMachine.OpenSubKey(registry_key))
             {
                 foreach (string subkey_name in key.GetSubKeyNames())
                 {
                     using (RegistryKey subkey = key.OpenSubKey(subkey_name))
                     {
-                        Console.WriteLine(subkey.GetValue("DisplayName"));
+                        Console.WriteLine(subkey.GetValue("DisplayName")); //debug.
                         try
                         {
                             listBox1.Items.Add(subkey.GetValue("DisplayName"));
+                            i ++;
                         }
                         catch
                         {
-                            continue;
+                            continue; //value is assumed null
                         }
                     }
                 }
+                label1.Text = i.ToString();
             }
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            selected = listBox1.SelectedItem.ToString();
+            MessageBox.Show(selected);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
